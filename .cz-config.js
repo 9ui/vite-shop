808730513192c4f0982f7f644f900bc51e969cb6
9ui <zzw9059190@gmail.com>
2020-11-13 22:03:33 +0800

    ✨特性: 提交规范

--- .cz-config.js
+++ .cz-config.js
@@ -1,15 +1,14 @@
 module.exports = {
   types: [
-    { value: '✨特性', name: '特性:    一个新的特性' },
-    { value: '✨功能', name: '功能:    一个新的功能' },
-    { value: '🐛修复', name: '修复:    修复一个Bug' },
-    { value: '📝文档', name: '文档:    变更的只有文档' },
-    { value: '💄格式', name: '格式:    空格, 分号等格式修复' },
-    { value: '♻️重构', name: '重构:    代码重构，注意和特性、修复区分开' },
-    { value: '⚡️性能', name: '性能:    提升性能' },
-    { value: '✅测试', name: '测试:    添加一个测试' },
-    { value: '🔧工具', name: '工具:    开发工具变动(构建、脚手架工具等)' },
-    { value: '⏪回滚', name: '回滚:    代码回退' },
+    { value: '✨特性', name: 'feat:    一个新的特性' },
+    { value: '🐛修复', name: 'fix:    修复一个Bug' },
+    { value: '📝文档', name: 'docs:    变更的只有文档' },
+    { value: '💄格式', name: 'style:    空格, 分号等格式修复' },
+    { value: '♻️重构', name: 'refactor:    代码重构，注意和特性、修复区分开' },
+    { value: '⚡️性能', name: 'perf:    提升性能' },
+    { value: '✅测试', name: 'test:    添加一个测试' },
+    { value: '🔧工具', name: 'chore:    开发工具变动(构建、脚手架工具等)' },
+    { value: '⏪回滚', name: 'revert:    代码回退' },
   ],
   scopes: [{ name: '其他修改' }, { name: '代码合并' }, { name: '样式修复' }],
   // it needs to match the value for field type. Eg.: 'fix'
@@ -34,7 +33,7 @@ module.exports = {
     confirmCommit: '确定提交说明?',
   },
   allowCustomScopes: true,
-  allowBreakingChanges: ['特性', '修复'],
+  allowBreakingChanges: ['feat', 'fix'],
   // limit subject length
   subjectLimit: 100,
 };
